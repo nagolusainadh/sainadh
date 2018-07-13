06d066fe6805cb603d54ef8e15ce7972e5a86a22
nagolusainadh <40800727+nagolusainadh@users.noreply.github.com>
2018-07-13 23:25:10 +0530

    Create matrhunt.c

---
+++ matrhunt.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+  int n,k,a[40][40],i,j,r,v,chk=0;
+scanf("%d%d",&n,&k);
+for(i=0;i<n;i++)
+{
+for(j=0;j<k;j++)
+{
+scanf("%d",&a[i][j]);
+}
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<k;j++)
+{
+if(a[i][j]==0)
+{ 
+r=i;
+v=j;
+for(v=0;v<k;v++)
+{
+a[i][v]=0;
+}
+for(r=0;r<n;r++)
+{ 
+a[r][j]=0; 
+chk=1; 
+}
+}
+if(chk==1)
+break;  
+}
+}
+for(i=0;i<n;i++)
+{
+if(i!=0)
+{
+printf("\n");
+}
+for(j=0;j<k;j++)
+{
+if(j==0)
+printf("%d",a[i][j]);
+else
+printf(" %d",a[i][j]);      
+}
+}
+return 0;
+}
