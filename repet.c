b38cb76428d0a71aa699d727e4ab44d31b0d5aa2
nagolusainadh <40800727+nagolusainadh@users.noreply.github.com>
2019-08-15 11:09:58 +0530

    Create repet.c

---
+++ repet.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+ int n,i,b=0;
+ int arr[100];
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&arr[i]);
+ }
+ int j;
+ for(i=0;i<n;i++)
+ {
+  for(j=1;j<n-1;j++)
+  {
+   if(arr[i]==arr[j])
+   {
+    b=arr[i];
+    printf("%d",b);
+    break;
+   }
+  }
+ }
+ if(b==0)
+ printf("Unique");
+}
